2842d7e896e7a28e204c61a5d4513ec1e505ebb6
Narendran A I <narenkrithick@gmail.com>
2023-09-17 19:14:55 +0530

    success-message component complete with reset button and back to home button and some bug fix 2

--- src/app/services/axios-service.service.spec.ts
+++ src/app/services/axios-service.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { AxiosService} from './axios-service.service';
 
-describe('AxiosServiceService', () => {
+describe('AxiosService', () => {
   let service: AxiosService;
 
   beforeEach(() => {
